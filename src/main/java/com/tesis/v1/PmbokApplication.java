20eeee37c14d771bbd412efb643547a2f2d87bbc
christofer <cc@cc.com>
2021-03-06 16:30:07 -0500

    Inicio de proyecto

---
+++ src/main/java/com/tesis/v1/PmbokApplication.java
@@ -0,0 +1,13 @@
+package com.tesis.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmbokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmbokApplication.class, args);
+	}
+
+}
