a8c65162a048c8d43e03855dc08e779a7a42b7fa
christofer <cc@cc.com>
2021-04-24 22:25:44 -0500

    aaaa

---
+++ src/main/java/com/tesis/v1/dto/pdp/entradasDTO.java
@@ -0,0 +1,61 @@
+package com.tesis.v1.dto.pdp;
+
+public class entradasDTO {
+	
+	private Integer idpdp;
+	private Integer identradapdp ;
+	private String otrosprocesos ;
+	private String factoresambientales ;
+	private String  activosprocesos;
+	
+	public entradasDTO(Integer idpdp, Integer identradapdp, String otrosprocesos, String factoresambientales,
+			String activosprocesos) {
+		this.idpdp = idpdp;
+		this.identradapdp = identradapdp;
+		this.otrosprocesos = otrosprocesos;
+		this.factoresambientales = factoresambientales;
+		this.activosprocesos = activosprocesos;
+	}
+
+	public Integer getIdpdp() {
+		return idpdp;
+	}
+
+	public void setIdpdp(Integer idpdp) {
+		this.idpdp = idpdp;
+	}
+
+	public Integer getIdentradapdp() {
+		return identradapdp;
+	}
+
+	public void setIdentradapdp(Integer identradapdp) {
+		this.identradapdp = identradapdp;
+	}
+
+	public String getOtrosprocesos() {
+		return otrosprocesos;
+	}
+
+	public void setOtrosprocesos(String otrosprocesos) {
+		this.otrosprocesos = otrosprocesos;
+	}
+
+	public String getFactoresambientales() {
+		return factoresambientales;
+	}
+
+	public void setFactoresambientales(String factoresambientales) {
+		this.factoresambientales = factoresambientales;
+	}
+
+	public String getActivosprocesos() {
+		return activosprocesos;
+	}
+
+	public void setActivosprocesos(String activosprocesos) {
+		this.activosprocesos = activosprocesos;
+	}
+	
+
+}
